f7e0047483ec61f35751eced010b06e1c86fabb4
erlison61 <erlison.souza@academico.ifpb.edu.br>
2024-10-04 16:31:26 -0300

    definindo Dockerfile.prod na raiz

--- create-image.sh
+++ create-image.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-DIST_DIR="jenkins_home/workspace/project-test"
-
 IMAGE_NAME="projeto-web" 
-IMAGE_TAG="1.0"          
+IMAGE_TAG="1.0"    
 
-cd "$DIST_DIR" || { echo "Diretório não encontrado: $DIST_DIR"; exit 1; }
+echo "buildando a imgem com o Dockerfile.prod e criando um nome para imagem:"
+docker build -f Dockerfile.prod -t "$IMAGE_NAME:$IMAGE_TAG" .
 
-# Construa a imagem Docker
-docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
-
-# Exiba a lista de imagens
+echo "listando as imagem"
 docker images
 
+echo "rodando imagem"
 docker run -p 80:80 $IMAGE_NAME:$IMAGE_TAG
